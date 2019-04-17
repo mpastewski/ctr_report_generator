b6a77a661aff5f117ae7fe1151cf19d0d9964c43
Mateusz Pastewski <mateusz.pastewski@gmail.com>
2019-04-17 13:07:07 +0200

    indicators and sightings imported from APIs. Connect_guid connected with sha256, IPs, URLs associated with PC

--- ctr_report_generator.py
+++ ctr_report_generator.py
@@ -39,38 +39,71 @@ def enrich(ctr_token, observable, type):
 
     return response_json
 
+def get_indicators(response_json, vulnerability, malware):
+
+    for module in response_json['data']:
+        if len(module['data']) > 0:
+            if 'indicators' in module['data'] and module['data']['indicators']['count'] > 0:
+                docs = module['data']['indicators']['docs']
+                for doc in docs:
+                    
+                    tag = doc['tags'][0]
+                    value = doc['short_description']
+
+                    if tag == 'vulnerability' and value not in vulnerability:
+                        vulnerability.append(value)
+                    elif tag == 'malware' and value not in malware: 
+                        malware.append(value)
+
+def get_sightings(response_json, sha256, IPs, url):
+    for module in response_json['data']:
+        if len(module['data']) > 0:
+            if 'sightings' in module['data'] and module['data']['sightings']['count'] > 0:
+                docs = module['data']['sightings']['docs']
+                for doc in docs:
+                    for relation in doc['relations']:
+
+                        value = relation['related']['value'] 
+                        type = relation['related']['type'] 
+
+                        if type == 'sha256' and value not in sha256:
+                            sha256.append(value)
+                        elif type == 'ip' and value not in IPs:
+                            IPs.append(value)
+                        elif type == 'url' and value not in url:
+                            url.append(value)
+                        elif type != 'ip' and type != 'sha256' and type != 'url':
+                            print("----- ATTENTION ------- {} {}".format(type, value))
+                            #continue
+
+        else:
+            return (sha256, IPs)
+
+
+
 #TODO: You do not need two variables to run this fun
 def parse_PC_observable(connector_guid, response_json):
 #def parse_PC_observable(response_json):
 
     sha256 = []
     IPs = []
+    url = []
+    vulnerability = []
+    malware = []
 
     try:
-        for module in response_json['data']:
-            if len(module['data']) > 0:
-                if 'sightings' in module['data'] and module['data']['sightings']['count'] > 0:
-                    docs = module['data']['sightings']['docs']
-                    for doc in docs:
-                        for relation in doc['relations']:
-
-                            value = relation['related']['value'] 
-                            type = relation['related']['type'] 
-
-                            if type == 'sha256' and value not in sha256:
-                                sha256.append(value)
-                            elif type == 'ip' and value not in IPs:
-                                IPs.append(value)
-                            else:
-                                continue
-
-            else:
-                return (sha256, IPs)
+        get_indicators(response_json, vulnerability, malware)
+    except:
+        print("MY_ERROR")
+        assert(0)
+    
+    try:
+        get_sightings(response_json, sha256, IPs, url)
     except:
         print("Error")
-        #print(connector_guid)
+        print(connector_guid)
 
-    return (sha256, IPs)
+    return (sha256, IPs, url, vulnerability, malware)
  
 def get(url):
 
@@ -94,10 +127,10 @@ def main():
    for pc in computers['data']:
         connector_guid = pc['connector_guid']
         response_json = enrich(ctr_token, connector_guid, "amp_computer_guid")
-        (sha256, IPs) = parse_PC_observable(connector_guid, response_json)
-        #(sha256, IPs) = parse_PC_observable(response_json)
+        (sha256, IPs, url, vulnerability, malware) = parse_PC_observable(connector_guid, response_json)
+        #(sha256, IPs, vulnerability) = parse_PC_observable(response_json)
 
-        print('connector_guid {} sha256 {} IPs {}'.format(connector_guid, sha256, IPs))
+        print('connector_guid {} sha256 {} IPs {} url {} vulnerability {} malware {}'.format(connector_guid, sha256, IPs, url, vulnerability, malware))
 
 if __name__ == '__main__':
     main()
