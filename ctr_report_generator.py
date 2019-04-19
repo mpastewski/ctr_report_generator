6331ad8be8fbc719bb652ca2da952e74ade742f0
Mateusz Pastewski <mateusz.pastewski@gmail.com>
2019-04-19 17:57:38 +0200

    threat_intel, attack_patterns, tags

--- ctr_report_generator.py
+++ ctr_report_generator.py
@@ -1,3 +1,6 @@
+from urllib.parse import urlparse
+import time
+import ipaddress
 import requests
 import json
 
@@ -25,86 +28,7 @@ def get_list_of_computers():
     response = get(url)
     return response
 
-def enrich(ctr_token, observable, type):
-    
-    ctr_observe_url = "https://visibility.amp.cisco.com/iroh/iroh-enrich/observe/observables"
 
-    headers = {'Authorization':'Bearer {}'.format(ctr_token),
-               'Content-Type':'application/json',
-               'Accept':'application/json'}
-
-    payload = '[ {{ "value":"{}"'.format(observable) + ', "type":"{}"'.format(type) + ' } ]'
-    response = SESSION.post(url=ctr_observe_url, headers=headers, data=payload)
-    response_json = response.json()
-
-    return response_json
-
-def get_indicators(response_json, vulnerability, malware):
-
-    for module in response_json['data']:
-        if len(module['data']) > 0:
-            if 'indicators' in module['data'] and module['data']['indicators']['count'] > 0:
-                docs = module['data']['indicators']['docs']
-                for doc in docs:
-                    
-                    tag = doc['tags'][0]
-                    value = doc['short_description']
-
-                    if tag == 'vulnerability' and value not in vulnerability:
-                        vulnerability.append(value)
-                    elif tag == 'malware' and value not in malware: 
-                        malware.append(value)
-
-def get_sightings(response_json, sha256, IPs, url):
-    for module in response_json['data']:
-        if len(module['data']) > 0:
-            if 'sightings' in module['data'] and module['data']['sightings']['count'] > 0:
-                docs = module['data']['sightings']['docs']
-                for doc in docs:
-                    for relation in doc['relations']:
-
-                        value = relation['related']['value'] 
-                        type = relation['related']['type'] 
-
-                        if type == 'sha256' and value not in sha256:
-                            sha256.append(value)
-                        elif type == 'ip' and value not in IPs:
-                            IPs.append(value)
-                        elif type == 'url' and value not in url:
-                            url.append(value)
-                        elif type != 'ip' and type != 'sha256' and type != 'url':
-                            print("----- ATTENTION ------- {} {}".format(type, value))
-                            #continue
-
-        else:
-            return (sha256, IPs)
-
-
-
-#TODO: You do not need two variables to run this fun
-def parse_PC_observable(connector_guid, response_json):
-#def parse_PC_observable(response_json):
-
-    sha256 = []
-    IPs = []
-    url = []
-    vulnerability = []
-    malware = []
-
-    try:
-        get_indicators(response_json, vulnerability, malware)
-    except:
-        print("MY_ERROR")
-        assert(0)
-    
-    try:
-        get_sightings(response_json, sha256, IPs, url)
-    except:
-        print("Error")
-        print(connector_guid)
-
-    return (sha256, IPs, url, vulnerability, malware)
- 
 def get(url):
 
     try:
@@ -120,6 +44,118 @@ def get(url):
     except requests.exceptions.RequestException as e:
         return "Error {}".format(e)
 
+
+def enrich(ctr_token, observable, type):
+    
+    ctr_observe_url = "https://visibility.amp.cisco.com/iroh/iroh-enrich/observe/observables"
+
+    headers = {'Authorization':'Bearer {}'.format(ctr_token),
+               'Content-Type':'application/json',
+               'Accept':'application/json'}
+
+    payload = '[ {{ "value":"{}"'.format(observable) + ', "type":"{}"'.format(type) + ' } ]'
+    response = SESSION.post(url=ctr_observe_url, headers=headers, data=payload)
+    try:
+        response_json = response.json()
+    except:
+        print("FATAL ERROR on observable {}".format(observable))
+        response_json = ''
+
+    return response_json
+
+def get_indicators(module, vulnerability):
+
+    if 'indicators' in module['data'] and module['data']['indicators']['count'] > 0:
+        docs = module['data']['indicators']['docs']
+        for doc in docs:
+            
+            tag = doc['tags'][0]
+            value = doc['short_description']
+
+            if tag == 'vulnerability' and value not in vulnerability:
+                vulnerability.append(value)
+
+def analyse_artifact(response_json):
+    threat_intel = dict()
+    attack_patterns = []
+    tags = []
+    for module in response_json['data']:
+        if len(module['data']) > 0:
+            if 'judgements' in module['data'] and module['data']['judgements']['count'] > 0:
+                docs = module['data']['judgements']['docs']
+                for doc in docs:
+
+                    threat_feed = module['module']
+                    disposition = doc['disposition_name']
+                    threat_intel[threat_feed] = disposition
+                
+            if 'indicators' in module['data'] and module['data']['indicators']['count'] > 0:
+                docs = module['data']['indicators']['docs']
+                for doc in docs:
+                    description = doc['description']
+                    attack_patterns.append(description)
+                    
+                    try:
+                        t = doc['tags']
+                        for tag in t:
+                            if tag not in tags:
+                                tags.append(tag)
+                    except:
+                        continue
+
+    return (threat_intel, attack_patterns, tags)
+
+def get_sightings(ctr_token, module, sha256, IPs, url, benign):
+    
+    threat_intel = dict()
+    attack_patterns = []
+    tags = []
+
+    if 'sightings' in module['data'] and module['data']['sightings']['count'] > 0:
+        docs = module['data']['sightings']['docs']
+        for doc in docs:
+            for relation in doc['relations']:
+
+                value = relation['related']['value'] 
+                type = relation['related']['type'] 
+
+                if type == 'url':
+                    try:
+                        parsed_uri = urlparse(value)
+                        value = '{uri.scheme}://{uri.netloc}/'.format(uri=parsed_uri)
+                    except:
+                        print('=== URL Parse ERROR {}'.format(value))
+                        value = ''
+
+                if type == 'sha256' and value not in sha256 and value not in benign:
+                    reputation_json = enrich(ctr_token, value, 'sha256')
+                    (threat_intel, attack_patterns, tags) = analyse_artifact(reputation_json)
+                    if 'Malicious' in threat_intel.values():
+                        sha256.append(value)
+                    else:
+                        benign.append(value)
+                    
+                elif type == 'ip' and value not in IPs and value not in benign:
+                    if not ipaddress.ip_address(value).is_private:
+                        reputation_json = enrich(ctr_token, value, 'ip')
+                        (threat_intel, attack_patterns, tags) = analyse_artifact(reputation_json)
+                        if 'Malicious' in threat_intel.values():
+                            IPs.append(value)
+                        else:
+                            benign.append(value)
+
+                elif type == 'url' and value not in url and value not in benign:
+                    reputation_json = enrich(ctr_token, value, 'url')
+                    (threat_intel, attack_patterns, tags) = analyse_artifact(reputation_json)
+                    if 'Malicious' in threat_intel.values():
+                        url.append(value)
+                    else:
+                        benign.append(value)
+                elif type != 'ip' and type != 'sha256' and type != 'url':
+                    print("----- ATTENTION ------- {} {}".format(type, value))
+
+    return (threat_intel, attack_patterns, tags)
+
 def main():
 
    ctr_token = generate_ctr_token()
@@ -127,11 +163,38 @@ def main():
    for pc in computers['data']:
         connector_guid = pc['connector_guid']
         response_json = enrich(ctr_token, connector_guid, "amp_computer_guid")
-        (sha256, IPs, url, vulnerability, malware) = parse_PC_observable(connector_guid, response_json)
-        #(sha256, IPs, vulnerability) = parse_PC_observable(response_json)
 
-        print('connector_guid {} sha256 {} IPs {} url {} vulnerability {} malware {}'.format(connector_guid, sha256, IPs, url, vulnerability, malware))
+        threat_intel = dict()
+        attack_patterns = []
+        tags = []
+
+
+        sha256 = []
+        IPs = []
+        url = []
+        vulnerability = []
+        benign = []
+        
+        try:
+            for module in response_json['data']:
+                if len(module['data']) > 0:
+                    try:
+                        get_indicators(module, vulnerability)
+                    except:
+                        print("MY_ERROR")
+            
+                    try:
+                        (threat_intel, attack_pattern) = get_sightings(ctr_token, module, sha256, IPs, url, benign)
+                    except:
+                        print("Error")
+        except:
+            print("=== MODULE ERROR === connector_guid {}".format(connector_guid))
+
+
+        #if sha256 or IPs or url or vulnerability:
+        print('connector_guid {} sha256 {} IPs {} url {} vulnerability {} threat_intel {} attack_pattern {}'.format(connector_guid, sha256, IPs, url, vulnerability, threat_intel, attack_patterns))
 
 if __name__ == '__main__':
+    start_time = time.time()
     main()
-
+    print("--- %s seconds --- {}".format(time.time() - start_time))
